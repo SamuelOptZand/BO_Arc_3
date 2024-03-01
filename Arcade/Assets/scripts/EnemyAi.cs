bbe21e0bb80fc6ed63c68addd5cb60fdb6f05144
SamuelOptZand <36631@ma-web.nl>
2024-03-01 16:58:56 +0100

    enemy ai

--- Arcade/Assets/scripts/EnemyAi.cs
+++ Arcade/Assets/scripts/EnemyAi.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using TMPro;
 using UnityEngine;
 
 public class EnemyAi : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private GameObject target;
+    private Vector3 targetPosition;
+
     void Start()
     {
-        
+        target = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float step = 5f * Time.deltaTime;
+
+        targetPosition = new Vector3(target.transform.position.x, 1, target.transform.position.z);
+
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, step);
+
+        transform.LookAt(targetPosition);
     }
 }
+
+
