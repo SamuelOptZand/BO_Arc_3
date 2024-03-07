7932b7154775ec8fd9b9c397eb5bf5088ce10bfb
SamuelOptZand <36631@ma-web.nl>
2024-03-07 11:29:28 +0100

    updates

--- Arcade/Assets/scripts/PlayerController.cs
+++ Arcade/Assets/scripts/PlayerController.cs
@@ -1,34 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
-    //Maak 2 variabelen beschikbaar in de inspector
     [SerializeField] private float speed = 50f;
     [SerializeField] private float rotSpeed = 50f;
 
-    //Maak een variabele voor je rigidbody
+    private float RotationSpeed = 10f;
+
     private Rigidbody rb;
 
-    // Start is called before the first frame update
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
 
-        Vector3 positionUpdate = new Vector3 (Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0f, Input.GetAxis("Vertical") * speed * Time.deltaTime);
-        transform.position += positionUpdate; 
+        //Vector3 positionUpdate = new Vector3 (Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0f, Input.GetAxis("Vertical") * speed * Time.deltaTime);
+        //transform.position += positionUpdate;
 
-        transform.Rotate(new Vector3(0, Input.GetAxis("rotate") * rotSpeed * Time.deltaTime, 0));
-         
-        
+        //transform.Rotate(new Vector3(0, Input.GetAxis("rotate") * rotSpeed * Time.deltaTime, 0));
 
+        rb.transform.Translate(new Vector3(horizontal, 0f, vertical) * speed * Time.deltaTime);
+
+
+        //transform.Rotate(0, Input.GetAxis("Mouse X") * RotationSpeed, 0);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        speed = speed * -3f;
+    }
+    void OnTriggerExit(Collider other)
+    {
+        Debug.Log("test");
+        speed = 50f;
     }
 
+    
 }
+
 
