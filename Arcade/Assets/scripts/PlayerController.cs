c73bbfccc02f654ea084041e4957be7cd3493d05
SamuelOptZand <36631@ma-web.nl>
2024-03-08 15:54:53 +0100

    WOOOO geen casper het spook meer

--- Arcade/Assets/scripts/PlayerController.cs
+++ Arcade/Assets/scripts/PlayerController.cs
@@ -1,10 +1,14 @@
 using Unity.VisualScripting;
-using UnityEngine;
-using UnityEngine.UIElements;
 
+using UnityEngine;
+
+using UnityEngine.UIElements;
+ 
 public class PlayerController : MonoBehaviour
 {
+
     [SerializeField] private float speed = 50f;
+
     [SerializeField] private float rotSpeed = 50f;
 
     private float RotationSpeed = 10f;
@@ -17,32 +21,16 @@ public class PlayerController : MonoBehaviour
     }
 
 
-    void Update()
+    void FixedUpdate()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        float moveHorizontal = Input.GetAxis("Horizontal");
 
-        //Vector3 positionUpdate = new Vector3 (Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0f, Input.GetAxis("Vertical") * speed * Time.deltaTime);
-        //transform.position += positionUpdate;
+        float moveVertical = Input.GetAxis("Vertical");
 
-        //transform.Rotate(new Vector3(0, Input.GetAxis("rotate") * rotSpeed * Time.deltaTime, 0));
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
-        rb.transform.Translate(new Vector3(horizontal, 0f, vertical) * speed * Time.deltaTime);
-
-
-        //transform.Rotate(0, Input.GetAxis("Mouse X") * RotationSpeed, 0);
-    }
-    private void OnTriggerEnter(Collider other)
-    {
-        speed = speed * -3f;
-    }
-    void OnTriggerExit(Collider other)
-    {
-        Debug.Log("test");
-        speed = 50f;
+        rb.velocity = movement * speed;
     }
 
-    
 }
-
 
