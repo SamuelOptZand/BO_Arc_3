51bd832e66f924cae2457ca567e9a853e342d08e
SamuelOptZand <36631@ma-web.nl>
2024-03-28 10:15:56 +0100

    script fix

--- Arcade/Assets/scripts/animator/animationP.cs
+++ Arcade/Assets/scripts/animator/animationP.cs
@@ -12,25 +12,7 @@ public class animation : MonoBehaviour
     }
     void Update()
     {
-
         resetTriggers();
-        //if (Input.GetAxis("Vertical") > 0)
-        //{
-        //    ani.SetTrigger("Walk");
-
-        //    if (Input.GetAxis("Horizontal") > 0)
-        //    {
-        //        ani.SetTrigger("Walk");
-        //    }
-        //    else if(Input.GetAxis("Horizontal") < 0)
-        //    {
-
-        //    }
-        //    else
-        //    {
-
-        //    }
-        //}
 
         if (Input.GetAxis("Vertical") is < 0 or > 0 || Input.GetAxis("Horizontal") is < 0 or > 0 )
         {
@@ -40,38 +22,11 @@ public class animation : MonoBehaviour
         {
             ani.SetTrigger("Idle");
         }
-
-
-
-        ////Check voor verticale input
-        //if (Input.GetAxis("Vertical") > 0 || Input.GetAxis("Horizontal") > 0)
-        //{
-        //    //is de waarde groter dan 0 dan heb je een knop naar boven ingedrukt
-        //    //Roep de juiste trigger aan!
-        //    ani.SetTrigger("Walk");
-        //    ani.ResetTrigger("Idle");
-        //    ani.ResetTrigger("WalkR");
-        //}
-        //else if (Input.GetAxis("Vertical") < 0 || Input.GetAxis("Horizontal") < 0)
-        //{
-        //    ani.SetTrigger("WalkR");
-        //    ani.ResetTrigger("Idle");
-        //    ani.ResetTrigger("Walk");
-        //}
-        //else
-        //{
-        //    //is de waarde 0 dan heb je niets ingedrukt
-        //    //Roep de juiste trigger aan
-        //    ani.SetTrigger("Idle");
-        //    ani.ResetTrigger("Walk");
-        //    ani.ResetTrigger("WalkR");
-        //}
     }
 
     private void resetTriggers()
     {
         ani.ResetTrigger("Idle");
         ani.ResetTrigger("Walk");
-        ani.ResetTrigger("WalkR");
     }
 }
